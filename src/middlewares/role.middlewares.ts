de0e67dec677653cddb1fb46865f5e5f8c9642e7
vaidusdp <vaidus2608m@gmail.com>
2026-08-19 22:42:34 +0530

    add UserRole enum and role-based access control middleware

---
+++ src/middlewares/role.middlewares.ts
@@ -0,0 +1,45 @@
+import type { Response, NextFunction } from "express";
+import type { AuthRequest } from "./auth.middlewares.js";
+import { UserRole } from "../../generated/prisma/enums.js";
+import { prisma } from "../db/prisma.js"
+import { success } from "zod";
+
+export const requireRole = (...allowedRoles: UserRole[]) => {
+    return async(
+        req: AuthRequest,
+        res: Response,
+        next: NextFunction
+    ) => {
+        if(!req.userId){
+            return res.status(401).json({
+                success: false,
+                message: "Authentication Require"
+            });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.userId
+            },
+            select: {
+                role: true
+            }
+        });
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "User Not Found"
+            });
+        }
+
+        if(!allowedRoles.includes(user.role)){
+            return res.status(403).json({
+                success: false,
+                message: "You do not have permission to perform this action"
+            });
+        }
+
+        next();
+    }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
 import { register, login, profile } from "../controllers/user.controllers.js";
 import { authenticate } from "../middlewares/auth.middlewares.js";
+import { requireRole } from "../middlewares/role.middlewares.js";
+import { UserRole } from "../../generated/prisma/enums.js";
 
 const router = Router();
 
 router.post("/", register);
 router.post("/login", login);
 router.get("/me", authenticate, profile);
+
+router.get(
+    "/admin-test",
+    authenticate,
+    requireRole(UserRole.ADMIN),
+    (req, res) => {
+        return res.json({
+            success: true,
+            message: "Welcome Admin",
+        });
+    }
+)
 
 export default router;
